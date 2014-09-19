2c6171423363b7103ae7d267ea12967cb4a5547f
Mateusz Krawczyński <mateusz.krawczynski@uxdream.com>
2014-09-19 18:28:57 +0200

    Dokumentacja Point

--- scripts/gaslib/Point.js
+++ scripts/gaslib/Point.js
@@ -2,6 +2,14 @@ define([
     "gaslib/Angle"
 ], function(Angle){
 
+	/**
+	* Point - represents point (latitude, longitude)
+	* @class Point
+	* @constructor
+    * @param latitude
+    * @param longitude	
+	*/
+
     var Point = function(lat, lng){
         lat = lat || 0;
         lng = lng || 0;
@@ -13,12 +21,20 @@ define([
 
     Point.constructor = Point;
 
-
+	/**
+	* Latitude in radians
+	* @method latInRadians
+	* @returns latitude in radians
+	*/
     Point.prototype.latInRadians = function(){
         return Angle.degreeToRadian(this.lat);
     };
 
-
+	/**
+	* Longitude in radians
+	* @method lngInRadians
+	* @returns longitude in radians
+	*/
     Point.prototype.lngInRadians = function(){
         return Angle.degreeToRadian(this.lng);
     };
