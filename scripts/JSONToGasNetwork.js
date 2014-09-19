4db6687d7491bad2302bc14623ae8687d67fbcd3
Marcin Wilczynski <wilczynski.pgs@gmail.com>
2014-09-19 19:11:47 +0200

    Zadanie 4 niedokonczone

--- scripts/JSONToGasNetwork.js
+++ scripts/JSONToGasNetwork.js
@@ -1,9 +1,18 @@
 define([
-
+    "gaslib/Angle"
 ], function(){
     "use strict";
 
-    var JSONToGasNetwork = function(data){
+    var JSONToGasNetwork = function(data,map){
+        for( var i=0;i<networkData.pipes.length;i++)
+        {
+            var start = networkData.pipes[i].start;
+            var end = networkData.pipes[i].end;
+            converter(start,convert);
+            converter(end, convert);
+
+            map.drawLine(new Point(start.lat,start.lng), new Point(end.lat,end.lng), {});
+        }
     };
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,31 @@
 require([
     "networkData",
     "maplib/MapDrawer",
-    "gaslib/Point"
-], function(networkData, MapDrawer, Point){
+    "gaslib/Point",
+    "gaslib/Angle"
+], function(networkData, MapDrawer, Point,Angle){
+    var convert = Angle();
+    function converter(point)
+    {
+        console.log(convert);
+        if(point.unit == "rad")
+        {
+            point.lat = convert.radianToDegree(point.lat);
+            point.lng = convert.radianToDegree(point.lng);
+        }
+    };
     var mapSection = document.getElementById('map');
     var map = new MapDrawer(mapSection);
     map.drawLine(new Point(52.40,16.90), new Point(52.23,21.06), {});
+    for( var i=0;i<networkData.pipes.length;i++)
+    {
+        var start = networkData.pipes[i].start;
+        var end = networkData.pipes[i].end;
+        converter(start);
+        converter(end);
+
+        map.drawLine(new Point(start.lat,start.lng), new Point(end.lat,end.lng), {});
+    }
 
 });
 
