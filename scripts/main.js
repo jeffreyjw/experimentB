52e211fdbc61466fbf79ba9e60112504ab4cce83
Marcin Wilczynski <wilczynski.pgs@gmail.com>
2014-09-19 18:32:36 +0200

    Zadanie 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 require([
     "networkData",
-    "maplib/MapDrawer"
-], function(networkData, MapDrawer){
+    "maplib/MapDrawer",
+    "gaslib/Point"
+], function(networkData, MapDrawer, Point){
     var mapSection = document.getElementById('map');
-    new MapDrawer(mapSection);
+    var map = new MapDrawer(mapSection);
+    map.drawLine(new Point(52.40,16.90), new Point(52.23,21.06), {});
 
 });
 
